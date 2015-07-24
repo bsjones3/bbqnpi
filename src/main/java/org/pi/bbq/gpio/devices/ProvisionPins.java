e7c90152744ad8dbd5317d6f5e992d5e2561cc2c
mdwagner <mdwagner@g.coastal.edu>
2015-07-24 16:32:34 -0400

    changes: ProvisionPins.java TemperatureReader.java

--- src/main/java/org/pi/bbq/gpio/devices/ProvisionPins.java
+++ src/main/java/org/pi/bbq/gpio/devices/ProvisionPins.java
@@ -5,22 +5,47 @@ package org.pi.bbq.gpio.devices;
  */
 
 import com.pi4j.io.gpio.*;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ProvisionPins
 {
 
-	public static void main(String[] args) throws InterruptedException
+	public void getTemperature()
 	{
+		System.out.println("Starting...");
 		final GpioController gpio = GpioFactory.getInstance();
 
 		// Sensor 1
-		GpioPinDigitalOutput sck = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, "SCK");
-		GpioPinDigitalOutput cs = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_05, "CS", PinState.HIGH);
-		GpioPinDigitalInput so = gpio.provisionDigitalInputPin(RaspiPin.GPIO_06, "SO");
+		GpioPinDigitalOutput sck = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_24, "SCK");
+		GpioPinDigitalOutput cs = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_25, "CS", PinState.HIGH);
+		GpioPinDigitalInput so = gpio.provisionDigitalInputPin(RaspiPin.GPIO_26, "SO");
+
+		int bytesin = 0;
+		cs.setState(PinState.LOW);
+		for(int i = 0; i < 32; i++)
+		{
+			sck.setState(PinState.LOW);
+			bytesin = bytesin << 1;
+			if(so.getState() == PinState.HIGH)
+			{
+				bytesin = bytesin | 1;
+			}
+			sck.setState(PinState.HIGH);
+		}
+		cs.setState(PinState.HIGH);
+		System.out.println("Number: " + bytesin);
+		gpio.shutdown();
+		gpio.unprovisionPin(sck);
+		gpio.unprovisionPin(cs);
+		gpio.unprovisionPin(so);
+
+
+
 		// Sensor 2
-		GpioPinDigitalOutput sck2 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, "SCK2");
-		GpioPinDigitalOutput cs2 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_05, "CS2", PinState.HIGH);
-		GpioPinDigitalInput so2 = gpio.provisionDigitalInputPin(RaspiPin.GPIO_06, "SO2");
+		//GpioPinDigitalOutput sck2 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_27, "SCK2");
+		//GpioPinDigitalOutput cs2 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_28, "CS2", PinState.HIGH);
+		//GpioPinDigitalInput so2 = gpio.provisionDigitalInputPin(RaspiPin.GPIO_29, "SO2");
 	}
 
 
