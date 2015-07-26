d9d1553a496ee6105afb8c059339c39c1d8299d2
Jay Wagner <jay.wagner@gmail.com>
2015-07-26 14:46:10 -0400

    changes

--- src/main/java/org/pi/bbq/readers/SensorReader.java
+++ src/main/java/org/pi/bbq/readers/SensorReader.java
@@ -1,13 +1,15 @@
-package org.pi.bbq;
+package org.pi.bbq.readers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 
 @Component
+@EnableScheduling
 public class SensorReader {
 
 	@Autowired
